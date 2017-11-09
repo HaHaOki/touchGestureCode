d9dbf7e8e03edc624d0f025533d3f2fb4e88709d
Harry <havh1_16@uni.worc.ac.uk>
2017-11-09 15:42:38 +0000

    Additional Changes

--- js/index.js
+++ js/index.js
@@ -4,10 +4,15 @@ $(document).on("pagecreate","#pageone",function(){
   
   	$('#tapholdtext').on("taphold",function(){
     	$(this).hide();
- 	});                       
+ 	});
+    
+    $('#bringbacktaphold').on("tap",function(){
+       $('#tapholdtext').show(); 
+    });
 
 	$('#taptext').on("tap",function(){
     	alert("Stop tapping!");
+        $(this).css('color', 'red');
  	}); 
 
 	$('#swipetext').on("swipeleft",function(){
